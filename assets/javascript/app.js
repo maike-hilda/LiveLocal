28f49bd69f034147303d4a5d8d262749a0aaba82
maike-hilda <maikescherer@gmail.com>
2017-07-31 19:54:23 -0700

    added icon, database, and linked basic docs and APIs

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+$(document).ready({
+
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBeOFAweBtSqKpoKdQZpEDtNVjXJeH5rOw",
+    authDomain: "livelocal-1f386.firebaseapp.com",
+    databaseURL: "https://livelocal-1f386.firebaseio.com",
+    projectId: "livelocal-1f386",
+    storageBucket: "",
+    messagingSenderId: "185892349757"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+
+});
